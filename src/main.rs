51b7346b7c612f1f33eb4932abbd3d52b4054f18
Felix Lam <lslam2@yahoo.com.hk>
2024-05-24 22:48:37 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,16 @@ fn main() {
 
 fn get_user_guess() -> u32 {
     loop {
-        let mut guess = String::new();
+        let mut guess = String::new(); // guess string variable is allocated here
         println!("Please guess the number");
-        io::stdin().read_line(&mut guess).expect("Cannot read user input");
+        io::stdin().read_line(&mut guess).expect("Cannot read user input"); // pass guess variable as a mutable reference , a borrow happens here
         match guess.trim().parse() {
-            Ok(num) => return num,
+            Ok(num) => return num, // compiler knows the guess string is parsed to u32 by type inference
             Err(err) => {
                 println!("Error : {err}");
             }
         }
+        // guess variable is automatically here
     }
 }
 
